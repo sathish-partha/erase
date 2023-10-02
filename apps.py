542220d53262009f69ca47beda8228b80df0f029
sathish-partha <146692235+sathish-partha@users.noreply.github.com>
2023-10-02 11:55:19 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -6,8 +6,8 @@ def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
-#     return r.json()
-# lottie_coding = load_lottieurl("https://lottie.host/f3188099-228a-4cfe-bfbd-e3f5b88ed680/BpFet5AtOZ.json")
+    return r.json()
+lottie_coding = load_lottieurl("https://lottie.host/f3188099-228a-4cfe-bfbd-e3f5b88ed680/BpFet5AtOZ.json")
 img_lottie_animation = Image.open("lottie_animation.png")
 
 

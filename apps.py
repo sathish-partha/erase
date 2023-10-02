4ed9227d0c2c6c6827de4924444d10006b47ff4c
sathish-partha <146692235+sathish-partha@users.noreply.github.com>
2023-10-02 10:25:49 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -8,7 +8,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("D:\Subhashini\Datascience\erase\style.css")
+local_css("style.css")
 
 # local_css("D:\Subhashini\Datascience\erase\style.css")
 

090207c2865ef2e7abd53d90811f324bf4267173
sathish-partha <146692235+sathish-partha@users.noreply.github.com>
2023-10-02 12:49:16 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -102,7 +102,7 @@ with st.container():
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-    <form action="https://formsubmit.co/erasepestcontrolservices@gmail.com" method="POST">
+    <form action="https://formsubmit.co/sathishmech1810@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
